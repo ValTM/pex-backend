5b8b302be531e9d2e08e96a147c9b37c63e00620
Val TM <valeri4ew@yahoo.com>
2023-12-09 02:30:44 +0200

    Add HTTP server initialization

---
+++ http_server/server.go
@@ -0,0 +1,71 @@
+package http_server
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"pex-backend/counter_service"
+)
+
+func ServerInstance(serverAddress string) {
+	server := initServer(serverAddress)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("failed to server: %s\n", err)
+		}
+	}()
+	log.Println("started server")
+
+	<-done
+	log.Println("stopping server gracefully")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("failed to stop the server cleanly: %s", err)
+	}
+	log.Println("server is stopped")
+}
+
+func initServer(serverAddress string) *http.Server {
+	timeout := time.Minute
+
+	mainRouter := chi.NewRouter()
+	mainRouter.Use(middleware.Logger)
+	mainRouter.Use(middleware.RequestID)
+	mainRouter.Use(middleware.Recoverer)
+	mainRouter.Use(middleware.RealIP)
+	mainRouter.Use(middleware.AllowContentType("application/json"))
+	mainRouter.Use(middleware.Heartbeat("/ping"))
+	mainRouter.Use(middleware.Timeout(timeout))
+
+	r := chi.NewRouter()
+	counterService := &counter_service.CounterService{}
+	counterService.InitService(r)
+
+	mainRouter.Mount("/api/v1", r)
+
+	server := &http.Server{
+		Addr:              serverAddress,
+		ReadTimeout:       timeout,
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      timeout,
+		Handler:           mainRouter,
+	}
+
+	return server
+}
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"pex-backend/http_server"
+)
+
+const serviceAddress = ":10101"
+
+func main() {
+	ctx := context.Background()
+	cancellable, cancelFn := context.WithCancel(ctx)
+	defer cancelFn()
+	ctx, cancelFunc := context.WithTimeout(cancellable, 5*time.Second)
+	defer cancelFunc()
+
+	http_server.ServerInstance(serviceAddress)
+}
